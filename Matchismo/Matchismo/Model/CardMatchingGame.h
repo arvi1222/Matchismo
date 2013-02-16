173422d4a9fd21c486e273f68145ad1535325007
Arvi Punzalan <arvi1222@yahoo.com>
2013-02-17 01:14:15 +0900

    Finish Lecture 3 slides walkthrough
    Ready for Assignment 1

---
+++ Matchismo/Matchismo/Model/CardMatchingGame.h
@@ -0,0 +1,23 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by Arvi on 2/16/13.
+//  Copyright (c) 2013 ArviApps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Deck.h"
+
+@interface CardMatchingGame : NSObject
+
+- (id)initWithCardCount:(NSUInteger)cardCount
+             usingDeck:(Deck *)deck;
+
+- (void)flipCardAtIndex:(NSUInteger)index;
+
+- (Card *)cardAtIndex:(NSUInteger)index;
+
+@property (nonatomic, readonly) int score;
+
+@end
